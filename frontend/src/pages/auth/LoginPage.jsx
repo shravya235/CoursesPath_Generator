cc3865c4e24886cde99a1fd2411d98a5c3812625
shravya235 <shravyar235@gmail.com>
2025-10-04 19:19:15 +0530

    Made some corrections in Login and Register Page

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
     setError(''); // Clear previous errors
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/login', {
+      const res = await fetch('/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- frontend/src/pages/auth/RegisterPage.jsx
+++ frontend/src/pages/auth/RegisterPage.jsx
@@ -47,7 +47,7 @@ const RegisterPage = () => {
     }
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
