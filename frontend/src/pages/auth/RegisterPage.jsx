ccc44667d63bafbcd7776b782d49b2db4c453b54
shravya235 <shravyar235@gmail.com>
2025-10-06 22:23:27 +0530

    Fixed frontend

--- frontend/src/pages/auth/RegisterPage.jsx
+++ frontend/src/pages/auth/RegisterPage.jsx
@@ -59,7 +59,7 @@ const RegisterPage = () => {
     }
 
     try {
-      const res = await fetch('/api/auth/register', {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // Forward all requests starting with /api to your backend server
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    }
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    server: command === 'serve' ? {
+      proxy: {
+        // Forward all requests starting with /api to your backend server in development
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+        },
+      }
+    } : undefined
   }
 })
